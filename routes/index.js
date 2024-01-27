e9bf0736b46b395ecabbc8b72708ab81a6ba7aad
Jescel James <jesceljames@gmail.com>
2024-01-27 11:26:09 -0500

    updated index.js routes with api and  wrong route

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,13 @@
 const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use(
+  (req, res) => 
+    {
+      return res.send('Wrong Route!')
+    }
+);
 
 module.exports = router;
