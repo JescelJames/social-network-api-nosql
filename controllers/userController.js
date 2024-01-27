c702c06dbee04aa88095e2a5bbfb24494583d7dd
Jescel James <jesceljames@gmail.com>
2024-01-27 11:41:02 -0500

    update userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,25 @@
+const User = require('../models/User');
+
+module.exports = {
+
+  async getUsers(req, res) {
+    try{
+      const users = await User.find();
+      res.json(users);
+    }
+    catch (err) {
+      res.status(500).json(err);
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
