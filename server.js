d5573497a7305b225e00e8aa94e9f50a4994383a
Jescel James <jesceljames@gmail.com>
2024-01-27 07:23:46 -0500

    established connection

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // DEPENDENCIES / REQUIREMENTS __________
   const express = require('express');
   const db = require('./config/connection');
-  const routes = require('./routes');
+  // const routes = require('./routes');
 // APP/PORT ____________________________  
   const app  = express();
   const PORT = process.env.PORT || 3001;
@@ -9,7 +9,7 @@
 // MIDDLEWARES ___________________________
   app.use(express.urlencoded({ extended: true }));
   app.use(express.json());
-  app.use(routes);
+  // app.use(routes);
 
 // START SERVER
   db.once('open', () => {
